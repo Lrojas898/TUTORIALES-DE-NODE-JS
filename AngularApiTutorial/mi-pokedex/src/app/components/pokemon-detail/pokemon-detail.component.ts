59fa0de5d0ebc0b96ab6f00dca86320b7ac79fca
Luis Manuel Rojas <luis.manuel.rojas71@gmail.com>
2025-08-07 11:41:51 -0500

    feat/working app

--- AngularApiTutorial/mi-pokedex/src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ AngularApiTutorial/mi-pokedex/src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { PokemonService } from '../../services/pokemon.service';
 import { CommonModule } from '@angular/common';
@@ -6,27 +6,27 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-pokemon-detail',
   standalone: true,
-  imports: [CommonModule, RouterModule], // Importamos RouterModule para el botón de volver
+  imports: [CommonModule, RouterModule],
   templateUrl: './pokemon-detail.component.html',
   styleUrls: ['./pokemon-detail.component.css']
 })
 export class PokemonDetailComponent implements OnInit {
-  pokemon: any = null; // Usaremos 'any' para simplificar, aquí guardaremos los detalles
+  // Cambiar a signal
+  pokemon = signal<any>(null);
 
-  // Inyectamos ActivatedRoute para leer la URL y PokemonService para pedir los datos
   constructor(
     private route: ActivatedRoute,
     private pokemonService: PokemonService
   ) { }
 
   ngOnInit(): void {
-    // Obtenemos el parámetro 'name' de la URL
     const pokemonName = this.route.snapshot.paramMap.get('name');
-
-    // Si existe el nombre, pedimos los detalles al servicio
+    
     if (pokemonName) {
       this.pokemonService.getPokemonDetails(pokemonName).subscribe(details => {
-        this.pokemon = details;
+        // Actualizar el signal
+        this.pokemon.set(details);
+        console.log('Pokemon details loaded:', details);
       });
     }
   }
