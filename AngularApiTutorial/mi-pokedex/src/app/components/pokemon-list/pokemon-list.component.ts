b8ec5764816d23b0961ae3c4bd94962729f82dab
Luis Manuel Rojas <luis.manuel.rojas71@gmail.com>
2025-08-07 11:40:12 -0500

    feat/added new fuctionality

--- AngularApiTutorial/mi-pokedex/src/app/components/pokemon-list/pokemon-list.component.ts
+++ AngularApiTutorial/mi-pokedex/src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { PokemonService } from '../../services/pokemon.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
@@ -6,21 +6,21 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-pokemon-list',
   standalone: true,
-  imports: [CommonModule, RouterModule], // Importamos CommonModule para ngFor y RouterModule para routerLink
+  imports: [CommonModule, RouterModule],
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.css']
 })
 export class PokemonListComponent implements OnInit {
-  pokemonList: any[] = [];
+  // Cambiar a signal
+  pokemonList = signal<any[]>([]);
 
-  // Inyectamos nuestro servicio para usarlo
   constructor(private pokemonService: PokemonService) { }
 
-  // ngOnInit se ejecuta cuando el componente se inicia
   ngOnInit(): void {
     this.pokemonService.getPokemonList().subscribe(response => {
-      this.pokemonList = response.results;
-      console.log(this.pokemonList)
+      // Actualizar el signal
+      this.pokemonList.set(response.results);
+      console.log(this.pokemonList());
     });
   }
 }
--- AngularApiTutorial/mi-pokedex/src/main.ts
+++ AngularApiTutorial/mi-pokedex/src/main.ts
@@ -1,3 +1,4 @@
+import 'zone.js'; // ← Agregar esta línea de vuelta
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
